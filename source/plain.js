6ded7c25074169e02be59136ce67a8596e6f9708
Sergey <falkr4m@yandex.ru>
2021-08-25 18:40:03 +0300

    tests passed

--- source/plain.js
+++ source/plain.js
@@ -1,5 +1,19 @@
 'use strict'
 
-function plain(...arrays) {
-    
-}
+const plain = arrays => [].concat(...arrays.flat(Infinity));
+
+// function plain(...arrays) {
+//     return result.concat(arrays.flat(Infinity));
+// }
+
+// function plain(...arrays) {
+//     let result = [];
+//     for (let i = 0; i < arrays.length; i++) {
+//         if (arrays[i] === undefined) {
+//             continue;
+//         }
+//
+//         result = result.concat(arrays[i].flat(Infinity));
+//     }
+//     return result;
+// }

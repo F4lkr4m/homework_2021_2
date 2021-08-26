329392c7a04f1ff5d0190cdce52d69df66162205
Sergey <falkr4m@yandex.ru>
2021-08-27 00:00:32 +0300

    deleted commentary

--- source/plain.js
+++ source/plain.js
@@ -1,19 +1,3 @@
 'use strict'
 
 const plain = arrays => [].concat(...arrays.flat(Infinity));
-
-// function plain(...arrays) {
-//     return result.concat(arrays.flat(Infinity));
-// }
-
-// function plain(...arrays) {
-//     let result = [];
-//     for (let i = 0; i < arrays.length; i++) {
-//         if (arrays[i] === undefined) {
-//             continue;
-//         }
-//
-//         result = result.concat(arrays[i].flat(Infinity));
-//     }
-//     return result;
-// }

04a379566d18f72b6dba5f5e4515ad6f875d39f0
Sergey <falkr4m@yandex.ru>
2021-08-29 22:50:40 +0300

    written tests and added jsdoc

--- source/plain.js
+++ source/plain.js
@@ -1,3 +1,14 @@
 'use strict'
 
-const plain = arrays => [].concat(...arrays.flat(Infinity));
+/**
+ * Flattening objects in one array
+ *
+ * @param {array} arrays with inner arrays
+ * @returns {array | undefined} flattened array or if input is wrong returns undefined
+ */
+const plain = arrays => {
+    if (!Array.isArray(arrays)) {
+        return undefined;
+    }
+    return [].concat(...arrays.flat(Infinity));
+}
--- tests/plain.js
+++ tests/plain.js
@@ -36,4 +36,16 @@ QUnit.module('Тестируем функцию plain', function () {
 	QUnit.test('Работает с элементами разных типов', function (assert) {
 		assert.deepEqual(plain([ [ 'abcde' ], [ [ 'f' ], [ null, false ], [ NaN, NaN ], NaN ], -Infinity ]), [ 'abcde', 'f', null, false, NaN, NaN, NaN, -Infinity ]);
 	});
+
+	QUnit.test('Работает с объектами в массивах', function (assert) {
+		assert.deepEqual(plain([ [{name: 'John', phone: '123456789'}, {name: 'Sarah', phone: '987654321'}], {city: 'Moscow'} ]), [{name: 'John', phone: '123456789'}, {name: 'Sarah', phone: '987654321'}, {city: 'Moscow'}]);
+		assert.deepEqual(plain([ {name: 'John'}, {name: 'Sarah'}, [ [ {city: 'Moscow'} ], [] ]]), [ {name: 'John'}, {name: 'Sarah'}, {city: 'Moscow'} ]);
+		assert.deepEqual(plain([ {name: 'John'}, {}, [ {city: 'Moscow'} ]]), [ {name: 'John'}, {}, {city: 'Moscow'} ]);
+	});
+
+	QUnit.test('Различает неправильный ввод', function (assert) {
+		assert.deepEqual(plain({}), undefined);
+		assert.deepEqual(plain('Hello there!'), undefined);
+		assert.deepEqual(plain(123), undefined);
+	});
 });
